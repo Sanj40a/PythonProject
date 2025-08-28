3ee9294580dbcaf6080bd33235bbb44f5265df0c
sanjay-chalise <sanjchalise@gmail.com>
2025-08-28 15:22:12 +0300

    First commit

--- sanj.py
+++ sanj.py
@@ -1,2 +1,5 @@
-print('heelo')
-
+fahrenheit_str = input("Enter a temperature in Fahrenheit: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("The temperature in Celsius: " + str(celsius))
+print(f"The temperature in Celsius: {celsius:.2f}")
